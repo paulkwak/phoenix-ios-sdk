70a667473131337a1c69a8f0b3daf5856eee4368
Steven Zhang <steven.zhang@tigerspike.com>
2014-03-18 10:31:20 +0800

    stop tracking Example/Pods and Example/Podfile.lock

--- Example/Pods/Pods-AFOAuth2Client@phoenixplatform-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AFOAuth2Client_phoenixplatform : NSObject
-@end
-@implementation PodsDummy_Pods_AFOAuth2Client_phoenixplatform
-@end
--- Example/Pods/Pods-TSPhoenix-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TSPhoenix : NSObject
-@end
-@implementation PodsDummy_Pods_TSPhoenix
-@end
