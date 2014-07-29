0b63aef75283d2ed40718e070105c4db42532b5f
Son Nguyen <son.nguyen@tigerspike.com>
2014-07-29 12:01:11 +1000

    Splitting up the screens

---
+++ Example/Example/src/UI/HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  Example
+//
+//  Created by Son Nguyen on 29/07/2014.
+//  Copyright (c) 2014 Tigerspike Products. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeViewController : UIViewController
+
+@end
